d63b000421f69f7428914628534d135003c7a916
Daniel Joanol <d.joanol@cleverpy.com>
2022-10-11 15:19:09 +0200

    First commit

---
+++ src/main/java/com/danieljoanol/pgsqlpopulator/PgsqlpopulatorApplication.java
@@ -0,0 +1,13 @@
+package com.danieljoanol.pgsqlpopulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgsqlpopulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgsqlpopulatorApplication.class, args);
+	}
+
+}
