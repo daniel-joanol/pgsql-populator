67447b9c50b274992b4f06ee8bf02a5c90a6ae39
Daniel Joanol <d.joanol@cleverpy.com>
2022-11-09 10:00:13 +0100

    Adds @CrossOrigin to controller

--- src/main/java/com/danieljoanol/pgsqlpopulator/config/SecurityConfig.java
+++ src/main/java/com/danieljoanol/pgsqlpopulator/config/SecurityConfig.java
@@ -13,7 +13,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests((authz) -> authz
-                .anyRequest().denyAll()
+                .anyRequest().permitAll()
             )
             .httpBasic();
         return http.build();
--- src/main/java/com/danieljoanol/pgsqlpopulator/controller/PopulatorController.java
+++ src/main/java/com/danieljoanol/pgsqlpopulator/controller/PopulatorController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @Controller
 @RequestMapping("/api/v1/populator")
 @Api(value = "Populator Controller", description = "PgSQL Populator Api")
+@CrossOrigin
 public class PopulatorController {
     
     @Autowired
